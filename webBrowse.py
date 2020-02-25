7fc9ebbd2433057eda80aa85344cd8a13ae4e32a
Ronny <ronnyangelo.freites@gmail.com>
2020-02-25 10:18:36 -0600

    add selenium basic actions

---
+++ webBrowse.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+messageField = driver.find_element_by_xpath('//*[@id="user-message"]')
+messageField.send_keys('Hello World')
+showMessageButton = driver.find_element_by_xpath('//*[@id="get-input"]/button')
+showMessageButton.click()
+
+additionField1 = driver.find_element_by_xpath('//*[@id="sum1"]')
+additionField1.send_keys('10')
+additionField2 = driver.find_element_by_xpath('//*[@id="sum2"]')
+additionField2.send_keys('1')
+getTotalButton = driver.find_element_by_xpath('//*[@id="gettotal"]/button')
+getTotalButton.click()
